816c8296c9338f630a491e8ea8d5d9de4fed8b4e
majakwani <anasjakwani72@gmail.com>
2022-09-07 12:01:01 +0500

    All the functionalities of the game is happening here

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,30 +1,75 @@
 import React from "react";
 import Die from "./Die";
 import Top from "./Top";
-
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 
 export default function App(){
   const [dice, setDice] = React.useState(allNewNumbers())
+
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(()=> {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allValue = dice.every(die => die.value === firstValue)
+
+    if(allHeld && allValue){
+      setTenzies(true)
+    }
+    }
+  , [dice])
 
   function allNewNumbers(){
     const numberArr = []
     for(let i = 0; i < 10; i++){
       numberArr.push({
         value: Math.floor(Math.random() *6) + 1,
-        isHeld: false 
+        isHeld: false, 
+        id: nanoid()
       })
   }
   return numberArr
   }
   
-  function handleEvent(){
-    setDice(allNewNumbers())
+  function generateNewDie(){
+    return {
+      value: Math.floor(Math.random() *6) + 1,
+      isHeld: false,
+      id: nanoid() 
+    }
   }
+
+  function handleEvent(){
+    if(tenzies){
+    setTenzies(false)
+    setDice(allNewNumbers())
+    }
+    else{
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? 
+      die :
+      generateNewDie()
+    }))
+  }
+  }
+
+  function holdDice(id){
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? 
+      {...die, isHeld: !die.isHeld} :
+      die
+    }))
+    }
 
   const diceElements = dice.map((die) => {
     return <Die 
-    property = {die} />
+    value = {die.value}
+    isHeld = {die.isHeld}
+    key = {die.id}
+    id = {die.id}
+    toggler = {holdDice} />
   })
 
 
@@ -34,10 +79,13 @@ export default function App(){
       <Top />
 
       <div className="dieContainer">
+      {tenzies &&
+       <Confetti width = {1920} height = {929} />
+       }
         {diceElements}
       </div>
 
-      <button type="button" onClick={handleEvent}>Roll</button>
+      <button type="button" onClick={handleEvent}>{tenzies ? "New Game" : "Roll"}</button>
 
     </main>
   )
