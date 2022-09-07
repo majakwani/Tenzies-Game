ca5691c6440950cd66477d81efcdd1b72a90a097
majakwani <anasjakwani72@gmail.com>
2022-09-07 12:01:31 +0500

    All the styling and rendering related to a Die is written here

--- Tenzies/src/Die.jsx
+++ Tenzies/src/Die.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Die(props){
     const styles = {
-        backgroundColor: props.property.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
-        <div className= "die" style={styles}>
-            <p>{props.property.value}</p>
+        <div onClick={() => props.toggler(props.id)} className= "die" style={styles}>
+            <p>{props.value}</p>
         </div>
     )
 }
